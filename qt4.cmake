318221cca143929e70741a24c45cbd526a3350d3
Carsten Haubold <carstenhaubold@googlemail.com>
2014-01-16 15:29:14 +0100

    Fix escaped ‘\’ in when creating symlinks for qt4 headers.

--- qt4.cmake
+++ qt4.cmake
@@ -98,7 +98,7 @@ ExternalProject_Add(${qt4_NAME}
 
 if (${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
 	ExternalProject_Add_Step(${qt4_NAME} ${qt4_NAME}-create-symlinks
-	   COMMAND "for i in `ls ${BUILDEM_DIR}/lib | grep \.framework`; do ln -s ${BUILDEM_DIR}/lib/$i/Headers ${BUILDEM_DIR}/include/`echo $i | sed 's/\.framework//g'`; done"
+	   COMMAND "for i in `ls ${BUILDEM_DIR}/lib | grep \\.framework`; do ln -s ${BUILDEM_DIR}/lib/$i/Headers ${BUILDEM_DIR}/include/`echo $i | sed 's/\\.framework//g'`; done"
 	   DEPENDEES install
 	)
 endif()
