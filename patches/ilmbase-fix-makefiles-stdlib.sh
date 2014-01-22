75db7486e11147a9c8ca8396c79d7f750d8f4723
Carsten Haubold <carstenhaubold@googlemail.com>
2014-01-22 09:28:21 +0100

    Apply a similar patch to openexr as it has the same makefile structure. Clean up the IlmPatch

--- patches/ilmbase-fix-makefiles-stdlib.sh
+++ patches/ilmbase-fix-makefiles-stdlib.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-echo "IlmBase source path: $1"
-
 sed -i .bkup '283s/-rpath/-Wc,-stdlib=libstdc\+\+ -rpath/g' $1/Half/Makefile
 sed -i .bkup '270s/-rpath/-Wc,-stdlib=libstdc\+\+ -rpath/g' $1/Iex/Makefile
 sed -i .bkup '290s/-rpath/-Wc,-stdlib=libstdc\+\+ -rpath/g' $1/Imath/Makefile
