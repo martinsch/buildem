3a9bd333cc71d07311a290e1359d9568c9996fde
Philipp Hanslovsky <Philipp.Hanslovsky@iwr.uni-heidelberg.de>
2014-01-23 10:59:41 +0100

    coorect name for boost random

--- boost.cmake
+++ boost.cmake
@@ -34,7 +34,7 @@ ExternalProject_Add(${boost_NAME}
     UPDATE_COMMAND      ""
     PATCH_COMMAND       ""
     CONFIGURE_COMMAND   ${BUILDEM_ENV_STRING} ./bootstrap.sh 
-        --with-libraries=date_time,filesystem,python,regex,serialization,system,test,thread,program_options,boost_random
+        --with-libraries=date_time,filesystem,python,regex,serialization,system,test,thread,program_options,random
         --with-python=${PYTHON_EXE} 
         --prefix=${BUILDEM_DIR}
         LDFLAGS=${BUILDEM_LDFLAGS}
