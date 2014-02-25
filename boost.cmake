06193073b513f6960a000fb4e54a3fcdb0c17787
hanslovsky <Philipp.Hanslovsky@gmail.com>
2014-02-25 13:18:56 +0100

    only add cxxflags to boost configuration if BUILDEM_ADDITIONAL_CXX_FLAGS is set with @chaubold

--- boost.cmake
+++ boost.cmake
@@ -43,6 +43,11 @@ endif()
 # some libraries require.  TODO: Possibly shore up all library find paths to only
 # allow use of built libs.
 message ("Installing ${boost_NAME} into FlyEM build area: ${BUILDEM_DIR} ...")
+if(BUILDEM_ADDITIONAL_CXX_FLAGS)
+  set(CXXFLAGS_LINE cxxflags="${BUILDEM_ADDITIONAL_CXX_FLAGS}" linkflags="${BUILDEM_ADDITIONAL_CXX_FLAGS}")
+else(BUILDEM_ADDITIONAL_CXX_FLAGS)
+  set(CXXFLAGS_LINE "")
+endif(BUILDEM_ADDITIONAL_CXX_FLAGS)
 ExternalProject_Add(${boost_NAME}
     DEPENDS             ${python_NAME} ${zlib_NAME}
     PREFIX              ${BUILDEM_DIR}
@@ -55,14 +60,14 @@ ExternalProject_Add(${boost_NAME}
         --with-python=${PYTHON_EXE} 
         --prefix=${BUILDEM_DIR}
     BUILD_COMMAND       ${BUILDEM_ENV_STRING} ./b2
-    	cxxflags="${BUILDEM_ADDITIONAL_CXX_FLAGS}" linkflags="${BUILDEM_ADDITIONAL_CXX_FLAGS}"
+    	"${CXXFLAGS_LINE}"
         --layout=tagged
         -sNO_BZIP2=1 
         -sZLIB_INCLUDE=${BUILDEM_DIR}/include 
         -sZLIB_SOURCE=${zlib_SRC_DIR} install
     BUILD_IN_SOURCE     1
     INSTALL_COMMAND     ${BUILDEM_ENV_STRING} ./b2
-    	cxxflags="${BUILDEM_ADDITIONAL_CXX_FLAGS}" linkflags="${BUILDEM_ADDITIONAL_CXX_FLAGS}"
+    	"${CXXFLAGS_LINE}"
         -sNO_BZIP2=1 
         -sZLIB_INCLUDE=${BUILDEM_DIR}/include 
         -sZLIB_SOURCE=${zlib_SRC_DIR} install
