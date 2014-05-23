a65c5efcb12ef9a6c9050804f2396b9c9f10cd3f
Carsten Haubold <carstenhaubold@googlemail.com>
2014-05-23 14:00:53 +0200

    Fix boost compile errors with Clang 3.4
    Conflicts:
    	boost.cmake

--- boost.cmake
+++ boost.cmake
@@ -9,6 +9,7 @@ CMAKE_MINIMUM_REQUIRED(VERSION 2.8)
 include (ExternalProject)
 include (BuildSupport)
 include (ExternalSource)
+include (PatchSupport)  # Using PATCH_EXE so this include should be here.
 
 include (python)
 include (zlib)
@@ -37,7 +38,6 @@ if(${BUILDEM_ADDITIONAL_CXX_FLAGS}} MATCHES "stdlib")
 	SET(boost_FLAGS cxxflags=${BUILDEM_ADDITIONAL_CXX_FLAGS} linkflags=${BUILDEM_ADDITIONAL_CXX_FLAGS})
 endif()
 	
-	
 
 # Add layout=tagged param to first boost install to explicitly create -mt libraries
 # some libraries require.  TODO: Possibly shore up all library find paths to only
@@ -54,7 +54,10 @@ ExternalProject_Add(${boost_NAME}
     URL                 ${boost_URL}
     URL_MD5             ${boost_MD5}
     UPDATE_COMMAND      ""
-    PATCH_COMMAND       ""
+    PATCH_COMMAND       ${BUILDEM_ENV_STRING} ${PATCH_EXE}
+			# these two patches fix a problem with clang3.4
+			${boost_SRC_DIR}/boost/atomic/detail/gcc-atomic.hpp ${PATCH_DIR}/boost-clang34-part1.patch
+			${boost_SRC_DIR}/boost/atomic/detail/cas128strong.hpp ${PATCH_DIR}/boost-clang34-part2.patch
     CONFIGURE_COMMAND   ${BUILDEM_ENV_STRING} ./bootstrap.sh 
         --with-libraries=date_time,filesystem,python,regex,serialization,system,test,thread,program_options,chrono,atomic
         --with-python=${PYTHON_EXE} 
